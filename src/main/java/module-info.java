8db852f69571117ad17d811b481cdfe7339650ad
Roy Duijndam <Roy.Duijndam1@gmail.com>
2021-12-09 10:31:21 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.challenge.the_challenge {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.challenge.the_challenge to javafx.fxml;
+    exports com.challenge.the_challenge;
+}
