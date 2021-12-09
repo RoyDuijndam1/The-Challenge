27fb84954d55716671f48b681ad43d0fc04f4a91
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2021-12-09 15:40:19 +0100

    Project to 1 folder

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,10 @@
-module com.challenge.the_challenge {
+module sample.challengeapp {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
+    requires javafx.graphics;
+    requires java.sql;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
 
-    opens com.challenge.the_challenge to javafx.fxml;
-    exports com.challenge.the_challenge;
+    opens challengeapp to javafx.fxml;
+    exports challengeapp;
 }
