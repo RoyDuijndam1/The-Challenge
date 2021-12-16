636d67ebc7380d30a22b4e55edefa17a761cf07d
Roy Duijndam <Roy.Duijndam1@gmail.com>
2021-12-16 10:31:21 +0100

    Add account_gegevens.fxml to project

--- AccountGegevensFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.accountgegevensfx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.accountgegevensfx to javafx.fxml;
-    exports com.example.accountgegevensfx;
-}
