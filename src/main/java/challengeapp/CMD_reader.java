dd0e975eff2e45f84fb436fe89b5e33a9faf7dc3
Quinten36 <q.kempers@mcw.nl>
2021-12-23 18:42:41 +0100

    SQL functies part twee. Hopefully

---
+++ src/main/java/challengeapp/CMD_reader.java
@@ -0,0 +1,120 @@
+package challengeapp;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.*;
+import java.util.Date;
+import java.util.UUID;
+
+public class CMD_reader extends Main {
+
+//    private static String uuidAsString;
+
+    public static String tail2(File file, int lines) {
+        java.io.RandomAccessFile fileHandler = null;
+        try {
+            fileHandler =
+                    new java.io.RandomAccessFile( file, "r" );
+            long fileLength = fileHandler.length() - 1;
+            StringBuilder sb = new StringBuilder();
+            int line = 0;
+
+            for(long filePointer = fileLength; filePointer != -1; filePointer--){
+                fileHandler.seek( filePointer );
+                int readByte = fileHandler.readByte();
+
+                if( readByte == 0xA ) {
+                    if (filePointer < fileLength) {
+                        line = line + 1;
+                    }
+                } else if( readByte == 0xD ) {
+                    if (filePointer < fileLength-1) {
+                        line = line + 1;
+                    }
+                }
+                if (line >= lines) {
+                    break;
+                }
+                sb.append( ( char ) readByte );
+            }
+
+            String lastLine = sb.reverse().toString();
+            return lastLine;
+        } catch( java.io.FileNotFoundException e ) {
+            e.printStackTrace();
+            return null;
+        } catch( java.io.IOException e ) {
+            e.printStackTrace();
+            return null;
+        }
+        finally {
+            if (fileHandler != null )
+                try {
+                    fileHandler.close();
+                } catch (IOException e) {
+                }
+        }
+    }
+
+    static int[] reverse(int a[], int n)
+    {
+        int[] b = new int[n];
+        int j = n;
+        for (int i = 0; i < n; i++) {
+            b[j - 1] = a[i];
+            j = j - 1;
+        }
+
+        // printing the reversed array
+//        System.out.println("Reversed array is: \n");
+//        for (int k = 0; k < n; k++) {
+//            System.out.println(b[k]);
+//        }
+        return b;
+    }
+
+
+    private static sqlFuncties sqlFile;
+    public CMD_reader(sqlFuncties sqlFile) {
+        this.sqlFile = sqlFile;
+    }
+
+    public static void main (String[] args) {
+        int teller = 0;
+        int[] values = new int[2];
+        File file = new File("G:\\logs\\log.txt");
+
+        String array = tail2(file, 2);
+        String strArray[] = array.split("\\n");
+////        System.out.println(strArray);
+//        for (int i = 0; i < strArray.length; i++) {
+//            // Printing the elements of String array
+////            System.out.print(strArray[i] + ", \n");
+//            if (strArray[i].length() < 15) {
+//                String temp[] = strArray[i].split("\\.");
+//
+//            }
+//        }
+
+        for (int i = 0; i < strArray.length; i++) {
+            // Printing the elements of String array
+            System.out.print(strArray[i] + ", \n");
+
+            String temp[] = strArray[i].split(" ");
+            String temp2[] = temp[1].split("\\.");
+
+            System.out.println("after: "+Integer.valueOf(temp2[0]));
+            values[teller] = Integer.valueOf(temp2[0]);
+            teller++;
+        }
+
+
+        System.out.println(sqlFile.insertIntoDB(values[0], values[1]));
+//        System.out.println(insertIntoDB(values[0], values[1]));
+    }
+}
---
+++ src/main/java/challengeapp/sqlFuncties.java
@@ -0,0 +1,53 @@
+package challengeapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class sqlFuncties {
+    public static void Main(String[] args) {
+        System.out.println("SQL file running");
+
+    }
+    public static boolean insertIntoDB(int tvoc, int eco2) {
+        boolean state = false;
+
+        //uuid gen plaatsen
+        UUID uuid = UUID.randomUUID();
+        String uuidAsString = uuid.toString();
+
+        //if uuid needed to be checked
+//        UUID sameUuid = UUID.fromString(uuidAsString);
+//        assert sameUuid.equals(uuid);
+
+        //datum aanmaken
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateFormat = formatter.format(date);
+
+        String connectionString = "jdbc:mysql://127.0.0.1:3306/challenge";
+
+
+        try (Connection connection = DriverManager.getConnection(connectionString, "root", "");
+             Statement statement = connection.createStatement();) {
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "INSERT INTO `meting`(`metingID`, `TVOC`, `CO2 waarde`, `Datum meting`, `UserID`) VALUES ('"+uuid+"',"+tvoc+","+eco2+",'"+dateFormat+"','33436ceb-2799-4de3-9e98-a5511a4de32f')";
+            System.out.println(selectSql);
+            int i = statement.executeUpdate(selectSql);
+
+            if (i > 0) {
+                state = true;
+            } else {
+                state = false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return state;
+    }
+}
