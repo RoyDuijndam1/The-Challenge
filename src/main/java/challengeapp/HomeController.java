c836e00090d2d0a5d1bab13faf711be32b5bf97a
Quinten36 <q.kempers@mcw.nl>
2021-12-23 18:24:53 +0100

    SQL dingen gefixt

--- src/main/java/challengeapp/HomeController.java
+++ src/main/java/challengeapp/HomeController.java
@@ -16,28 +16,28 @@ public class HomeController extends SceneController{
     private Button nuMetenButton;
     @FXML
     void nuMetenButtonOnAction(ActionEvent event) {
-        luchtScore.setText("100");
+//        luchtScore.setText("100");
 
 
-//        String connectionString = "jdbc:mysql://localhost:3308/challenge";
-//        ResultSet resultSet = null;
-//
-//        try (Connection connection = DriverManager.getConnection(connectionString, "root", "");
-//             Statement statement = connection.createStatement();) {
-//
-//            // Create and execute a SELECT SQL statement.
-//            String selectSql = "SELECT score FROM account WHERE `userID`= '33436ceb-2799-4de3-9e98-a5511a4de32f'";
-//            resultSet = statement.executeQuery(selectSql);
-//
-//            // Print results from select statement
-//            while (resultSet.next()) {
-//                //System.out.println(resultSet.getString(8));
-//                luchtScore.setText(resultSet.getString(1));
-//                //System.out.println(resultSet);
-//            }
-//        } catch (SQLException er) {
-//            er.printStackTrace();
-//        }
+        String connectionString = "jdbc:mysql://127.0.0.1:3306/challenge";
+        ResultSet resultSet = null;
+
+        try (Connection connection = DriverManager.getConnection(connectionString, "root", "");
+             Statement statement = connection.createStatement();) {
+
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT score FROM account WHERE `userID`= '33436ceb-2799-4de3-9e98-a5511a4de32f'";
+            resultSet = statement.executeQuery(selectSql);
+
+            // Print results from select statement
+            while (resultSet.next()) {
+                //System.out.println(resultSet.getString(8));
+                luchtScore.setText(resultSet.getString(1));
+                //System.out.println(resultSet);
+            }
+        } catch (SQLException er) {
+            er.printStackTrace();
+        }
     }
 
     public void rankingButtonOnAction(MouseEvent event) throws IOException {
